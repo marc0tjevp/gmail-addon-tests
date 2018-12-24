6ce4a93292ed27f4694329d43f551a58f78a45b8
Marco van Poortvliet <Marc0tjevp@gmail.com>
2018-12-24 01:50:53 +0100

    Variables for saving email

--- controllers/email.controller.js
+++ controllers/email.controller.js
@@ -1,8 +1,22 @@
 const Email = require('../models/schemas/email.schema').Email
 
 let saveEmail = (req, res) => {
+
+    const id = req.body.id || ''
+    const sender = req.body.sender || ''
+    const name = req.body.name || ''
+    const subject = req.body.subject || ''
+    const body = req.body.body || ''
+    const date = req.body.date || ''
    
-    const email = new Email(req.body)
+    const email = new Email({
+        id: id,
+        sender: sender,
+        name: name,
+        subject: subject,
+        body: body,
+        date: date,
+    })
 
     email.save().then( res.status(200).json(email).end())
 
